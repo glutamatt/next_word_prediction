1acd0d9ee5c08c82a438dbc3c8cc535a174fd3a8
Glutamatt <glutamatt@live.fr>
2021-06-28 11:28:00 +0200

    std in out

--- poc.py
+++ poc.py
@@ -1,4 +1,6 @@
 # %%
+
+import sys
 import torch
 import string
 
@@ -11,9 +13,6 @@ from transformers import FlaubertTokenizer, FlaubertWithLMHeadModel
 flaubert_tokenizer = FlaubertTokenizer.from_pretrained('flaubert/flaubert_large_cased')
 flaubert_model = FlaubertWithLMHeadModel.from_pretrained('flaubert/flaubert_large_cased')
 
-top_k = 10
-
-
 def decode(tokenizer, pred_idx, top_clean):
     ignore_tokens = string.punctuation + '[PAD]'
     tokens = []
@@ -25,6 +24,9 @@ def decode(tokenizer, pred_idx, top_clean):
 
 
 def encode(tokenizer, text_sentence, add_special_tokens=True):
+    if text_sentence.find("<mask>") == -1:
+        text_sentence = text_sentence + " <mask>"
+
     text_sentence = text_sentence.replace('<mask>', tokenizer.mask_token)
     # if <mask> is the last token, append a "." so that models dont predict punctuation.
     if tokenizer.mask_token == text_sentence.split()[-1]:
@@ -32,26 +34,18 @@ def encode(tokenizer, text_sentence, add_special_tokens=True):
 
     input_ids = torch.tensor([tokenizer.encode(text_sentence, add_special_tokens=add_special_tokens)])
     mask_idx = torch.where(input_ids == tokenizer.mask_token_id)[1].tolist()[0]
-    return input_ids, mask_idx
+    return input_ids, mask_idx, text_sentence
 
 
 def get_all_predictions(text_sentence, top_clean=5):
-    # ========================= BERT =================================
-    #print(text_sentence)
-    #input_ids, mask_idx = encode(bert_tokenizer, text_sentence)
-    #with torch.no_grad():
-    #    predict = bert_model(input_ids)[0]
-    #bert = decode(bert_tokenizer, predict[0, mask_idx, :].topk(top_k).indices.tolist(), top_clean)
-
-    # ========================= FLAUBERT =================================
-    input_ids, mask_idx = encode(flaubert_tokenizer, text_sentence)
+    input_ids, mask_idx, text_sentence  = encode(flaubert_tokenizer, text_sentence)
     with torch.no_grad():
         predict = flaubert_model(input_ids)[0]
-    flaubert = decode(flaubert_tokenizer, predict[0, mask_idx, :].topk(top_k).indices.tolist(), top_clean)
+    flaubert = decode(flaubert_tokenizer, predict[0, mask_idx, :].topk(top_clean).indices.tolist(), top_clean)
 
-    return {
-        #'bert': bert,
-        'flaubert':flaubert,
-        }
+    return flaubert.splitlines(), text_sentence
 
-print(get_all_predictions("Génial ! Ca va être tellement <mask> ! J'adore ça."))
+for name in map(str.rstrip, sys.stdin):
+    preds, text_sentence = get_all_predictions(name, 50)
+    for p in preds:
+        print(text_sentence.replace(flaubert_tokenizer.mask_token, p))
